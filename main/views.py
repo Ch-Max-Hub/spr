7aa978b4cdde6bd2dfb8c8710b49b0134687f50b
Your Name Comes Here <you@yourdomain.example.com>
2024-03-05 11:18:08 +0500

    some little changes

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.shortcuts import render
+import pandas as pd
 
+from .forms import ExcelUploadForm
 from .models import Client, UserEtrap, Etrap
 
 
@@ -47,10 +49,28 @@ def add_info(request):
 
 
 def add_info_file(request):
+    if request.method == 'POST':
+        form = ExcelUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            excel_data = request.FILES['excel_file']
+            df = pd.read_excel(excel_data)  # Parse Excel file using pandas
+            for index, row in df.iterrows():
+                Client.objects.create(
+                    number=row['Number'],
+                    name=row['Name'],
+                    street=row['Street'],
+                    house=row['House'],
+                    # Add more fields as needed
+                )
+            return render(request, 'upload_form.html')
+    else:
+        form = ExcelUploadForm()
+
     context = {
         'title': '09 | Maglumat file ýükle',
+        'form': form,
     }
-    return render(request, 'index.html', context=context)
+    return render(request, 'upload_form.html', context=context)
 
 
 def dark_mode(request, turn_on):
